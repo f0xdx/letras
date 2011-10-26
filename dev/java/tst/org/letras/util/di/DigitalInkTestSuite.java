8d3402a9f46068a935355569b188cf9164ff0290
Felix Heinrichs <felix.heinrichs@cs.tu-darmstadt.de>
2011-10-26 12:35:06 +0200

    some minor changes in the license

---
+++ dev/java/tst/org/letras/util/di/DigitalInkTestSuite.java
@@ -0,0 +1,37 @@
+/*
+ * Build by TU Darmstadt 2011, all rights reserved.
+ */
+package org.letras.util.di;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author Felix Heinrichs <felix.heinrichs@cs.tu-darmstadt.de>
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({TraceTest.class})
+public class DigitalInkTestSuite {
+
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+}
---
+++ dev/java/tst/org/letras/util/di/TraceTest.java
@@ -0,0 +1,104 @@
+/*
+ * Build by TU Darmstadt 2011, all rights reserved.
+ */
+package org.letras.util.di;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Test for traces in the digital ink data structure.
+ * 
+ * @author Felix Heinrichs <felix.heinrichs@cs.tu-darmstadt.de>
+ * @version 0.3.0
+ */
+public class TraceTest {
+	
+	public TraceTest() {
+	}
+
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownClass() throws Exception {
+	}
+	
+	@Before
+	public void setUp() {
+	}
+	
+	@After
+	public void tearDown() {
+	}
+
+	/**
+	 * Test of getSamples method, of class Trace.
+	 */
+	@Test
+	public void testGetSamples() {
+		Trace instance = new Trace();
+		assertNotNull(instance.getSamples());
+	}
+
+	/**
+	 * Test of setSamples method, of class Trace.
+	 */
+	@Test
+	public void testSetSamples() {
+	}
+
+	/**
+	 * Test of timeFrame method, of class Trace.
+	 */
+	@Test
+	public void testTimeFrame() {
+	}
+
+	/**
+	 * Test of boundingBox method, of class Trace.
+	 */
+	@Test
+	public void testBoundingBox() {
+	}
+
+	/**
+	 * Test of pathLength method, of class Trace.
+	 */
+	@Test
+	public void testPathLength() {
+	}
+
+	/**
+	 * Test of computeBoundingBox method, of class Trace.
+	 */
+	@Test
+	public void testComputeBoundingBox() {
+	}
+
+	/**
+	 * Test of computeTimeFrame method, of class Trace.
+	 */
+	@Test
+	public void testComputeTimeFrame() {
+	}
+
+	/**
+	 * Test of computePathLength method, of class Trace.
+	 */
+	@Test
+	public void testComputePathLength() {
+	}
+
+	/**
+	 * Test of add method, of class Trace.
+	 */
+	@Test
+	public void testAdd() {
+	}
+}
